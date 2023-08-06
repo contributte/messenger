b4f3dbc5caa821f26bc3216ed16d5f86e0bbf1ff
Jan Mikeš <j.mikes@me.com>
2023-08-06 19:40:17 +0200

    Allow no senders by default

--- src/DI/MessengerExtension.php
+++ src/DI/MessengerExtension.php
@@ -89,7 +89,7 @@ class MessengerExtension extends CompilerExtension
 					'class' => null,
 					'autowired' => true,
 					'allowNoHandlers' => false,
-					'allowNoSenders' => false,
+					'allowNoSenders' => true,
 				],
 			])),
 			'serializer' => Expect::arrayOf(
