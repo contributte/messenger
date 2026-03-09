b6839e7ba5913df14b45ec140a3cdc3e9b5cf3d4
Milan Felix Šulc <rkfelix@gmail.com>
2026-03-09 15:38:23 +0100

    Feature: Doctrine transport factory auto-registration
    Register DoctrineTransportFactory automatically when ConnectionRegistry
    is available in the container. Move TransportFactory finalization to
    beforePassCompile to support late-registered factories.

--- src/DI/Pass/TransportFactoryPass.php
+++ src/DI/Pass/TransportFactoryPass.php
@@ -4,7 +4,10 @@ namespace Contributte\Messenger\DI\Pass;
 
 use Contributte\Messenger\DI\MessengerExtension;
 use Contributte\Messenger\DI\Utils\BuilderMan;
+use Doctrine\Persistence\ConnectionRegistry;
+use Nette\DI\Definitions\ServiceDefinition;
 use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpTransportFactory;
+use Symfony\Component\Messenger\Bridge\Doctrine\Transport\DoctrineTransportFactory;
 use Symfony\Component\Messenger\Bridge\Redis\Transport\RedisTransportFactory;
 use Symfony\Component\Messenger\Transport\InMemory\InMemoryTransportFactory;
 use Symfony\Component\Messenger\Transport\Sync\SyncTransportFactory;
@@ -18,6 +21,7 @@ class TransportFactoryPass extends AbstractPass
 		'inMemory' => InMemoryTransportFactory::class,
 		'amqp' => AmqpTransportFactory::class,
 		'redis' => RedisTransportFactory::class,
+		'doctrine' => DoctrineTransportFactory::class,
 	];
 
 	/**
@@ -28,8 +32,12 @@ class TransportFactoryPass extends AbstractPass
 		$builder = $this->getContainerBuilder();
 		$config = $this->getConfig();
 
-		// Filter out class factory that cannot be found
-		$defaultFactories = array_filter(self::DEFAULT_TRANSPORT_FACTORY, static fn ($class, $name) => class_exists($class), ARRAY_FILTER_USE_BOTH);
+		// Filter out class factory that cannot be found, exclude doctrine (requires ConnectionRegistry)
+		$defaultFactories = array_filter(
+			self::DEFAULT_TRANSPORT_FACTORY,
+			static fn ($class, $name) => class_exists($class) && $name !== 'doctrine',
+			ARRAY_FILTER_USE_BOTH,
+		);
 
 		// Merge default + user defined factories
 		$transportFactories = array_merge($defaultFactories, (array) $config->transportFactory);
@@ -41,8 +49,32 @@ class TransportFactoryPass extends AbstractPass
 				->addTag(MessengerExtension::TRANSPORT_FACTORY_TAG, $name);
 		}
 
+		// Placeholder: TransportFactory will be finalized in beforePassCompile
 		$builder->addDefinition($this->prefix('transportFactory'))
-			->setFactory(TransportFactory::class, [BuilderMan::of($this)->getTransportFactories()]);
+			->setFactory(TransportFactory::class, [[]]);
+	}
+
+	/**
+	 * Decorate services
+	 */
+	public function beforePassCompile(): void
+	{
+		$builder = $this->getContainerBuilder();
+
+		// Register Doctrine transport factory when ConnectionRegistry is available
+		if (class_exists(DoctrineTransportFactory::class) && interface_exists(ConnectionRegistry::class)) {
+			if ($builder->getByType(ConnectionRegistry::class, false) !== null) {
+				$builder->addDefinition($this->prefix('transportFactory.doctrine'))
+					->setFactory(DoctrineTransportFactory::class)
+					->setAutowired(false)
+					->addTag(MessengerExtension::TRANSPORT_FACTORY_TAG, 'doctrine');
+			}
+		}
+
+		// Finalize TransportFactory with all registered factories
+		/** @var ServiceDefinition $transportFactoryDef */
+		$transportFactoryDef = $builder->getDefinition($this->prefix('transportFactory'));
+		$transportFactoryDef->setArgument(0, BuilderMan::of($this)->getTransportFactories());
 	}
 
 }
